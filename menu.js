3f248c95a3b5283a107104adfa9fe42a6e3701cd
Joby Joseph <joby.joseph@nbcuni.com>
2018-07-27 14:57:54 -0700

    menu2

--- menu.js
+++ menu.js
@@ -1,16 +1,15 @@
 const { Menu } = require('electron')
 
 function createMenu() {
-    let template = [{
-        label: 'Hello'
-    }];
+    let template = [
+        { role: 'editMenu' },
+        { role: 'windowMenu' }
+    ];
     if (process.platform === 'darwin') {
         template.unshift({
             label: "Code Editor",
             submenu: [{
-                label: 'Quitter',
-                accelerator: 'CmdOrCtrl+Q',
-                click: () => app.quit()
+                role: 'quit'
             }]
         })
     }
