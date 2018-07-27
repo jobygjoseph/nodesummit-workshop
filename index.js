589b6e790202b02a76d15dfef46b8ca3999a26b2
Joby Joseph <joby.joseph@nbcuni.com>
2018-07-27 14:41:24 -0700

    drag file to editor

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ let myWindow;
 app.on('ready', () => {
     myWindow = new BrowserWindow()
     myWindow.loadFile('index.html')
+
+    myWindow.webContents.on('will-navigate', (e, url) => {
+        e.preventDefault()
+
+        // console.log(url, url.slice(7));
+
+        // IPC
+        
+        e.sender.send('open-file', url.slice(7)) // we don't want "file://" prefix
+    })
 });
 
 
--- renderer.js
+++ renderer.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
 const loader = require('monaco-loader');
+const { ipcRenderer } = require('electron');
 const $ = document.querySelector.bind(document);
 console.log('👺');
 
 let editor
+
+ipcRenderer.on('open-file', ( e, filePath ) => openFile(filePath));
+
+function openFile(filePath) {
+    const content = fs.readFileSync(filePath, 'utf-8');
+
+    editor.setValue(content)
+}
 
 loader().then((monaco) => {
     const div = $('#container');
